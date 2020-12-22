1f0208cc24afd6dccefd6a5126e4ea7b1c773618
Tsegu Abera <abishay2020@gmail.com>
2020-12-22 09:24:02 -0500

    This is my first commit.

---
+++ src/Array.java
@@ -0,0 +1,36 @@
+ // Please show this code and print to the screen using "for loop"
+
+public class Array {
+
+	public static void main(String[] args) {
+		int ages[] = {4,5,3,5,7};                     // use when we have few arrays may be less than 10.
+		double heights[] = {4.7,4.5,3.9,5.0,6.5};
+		
+		//int ages[] = new int [5];
+		//double heights[] = new double[5];
+		
+		//ages[0] = 4;                   // use when we have many arrays. 
+		//ages[1] = 5;
+		//ages[2] = 3;
+		//ages[3] = 5;
+		//ages[4] = 7;
+		
+		System.out.println("The firs child age is: " + ages[0]);
+		System.out.println("The second child age is: " + ages[1]);
+		System.out.println("The third child age is: " + ages[2]);
+		System.out.println("The fourth child age is: " + ages[3]);
+		System.out.println("The fith child age is: " + ages[4]);
+		
+		//heights[0] = 4.7;
+		//heights[1] = 4.5;
+		//heights[2] = 3.9;
+		//heights[3] = 5.0;
+		//heights[4] = 6.5;
+		
+		System.out.println("\n\nThe first child height is: " + heights[0]);
+		System.out.println("The second child height is: " + heights[1]);
+		System.out.println("The third child height is: " + heights[2]);
+
+	}
+
+}
